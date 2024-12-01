f29d0bb0787583e5cd6b576607c3fcf831f7cdc3
freiremat <urbsfreire@gmail.com>
2024-12-01 20:14:56 -0300

    tasks: - create book-detail comp

---
+++ src/app/book-catalog/book-detail/book-detail.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from 'src/app/model/book';
+import { BookListService } from 'src/app/services/BookList.service';
+
+@Component({
+  selector: 'app-book-detail',
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.scss']
+})
+export class BookDetailComponent {
+  bookId: number = 0;
+  book: Book | null = null;
+
+  constructor(public route: ActivatedRoute, private bkService: BookListService) { }
+
+  ngOnInit() {
+    // Captura o ID do livro da rota
+    this.route.params.subscribe(params => {
+      this.bookId = +params['id']; // O '+' converte para number
+      this.loadBook();
+    });
+  }
+
+  loadBook() {
+    this.book = this.bkService.getBooks().find(book => book.id === this.bookId) || null;
+  }
+}
